8ad8559c58a9965f46189eafcd21befd6ecf100d
Santosh Ray <santosh.ray81@gmail.com>
2020-05-24 21:42:44 -0700

    Added corner testcase for file recursion problem and also uploaded the test directories

--- P1/problem_2_file_recursion.py
+++ P1/problem_2_file_recursion.py
@@ -1,6 +1,5 @@
 import os
 
-
 def find_files(suffix,path):
 
 	file_list=[]
@@ -8,8 +7,13 @@ def find_files(suffix,path):
 		if(path.endswith(".c")):
 			file_list += [path]
 		return file_list
- 
-	dir_list = os.listdir(path)
+	
+
+	try:
+		dir_list = os.listdir(path)
+	except:
+		dir_list = []
+
 	# Let us print the files in the directory in which you are running this script
 	for p in dir_list:
 
@@ -19,6 +23,30 @@ def find_files(suffix,path):
 
 
 if __name__ == '__main__':
+	print("---Test case 1---")
+
 	c_file_list  = find_files("","./testdir")
+	print("num files : {}".format(len(c_file_list)))
 	for c_file in c_file_list:
 		print (c_file)
+
+	print("---Test case 2---")
+	
+	c_file_list  = find_files("","")
+	print("num files : {}".format(len(c_file_list)))
+	for c_file in c_file_list:
+		print (c_file)		
+
+	print("---Test case 3---")
+	
+	c_file_list  = find_files("","./testdir2")
+	print("num files : {}".format(len(c_file_list)))
+	for c_file in c_file_list:
+		print (c_file)		
+
+	print("---Test case 4---")
+	
+	c_file_list  = find_files("","./testdir3")
+	print("num files : {}".format(len(c_file_list)))
+	for c_file in c_file_list:
+		print (c_file)		
